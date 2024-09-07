ea661bd87060abebebbe1f8f6e0bae0b4687ab6c
mvpcurtis <curtis.bowen@gmx.com>
2024-09-07 16:53:47 +0100

    Fourth commit, 	started work on JS file and implemented API key.

---
+++ weather-app/script.js
@@ -0,0 +1,6 @@
+// https://api.openweathermap.org/data/2.5/weather?id=london&appid=e19ae291c8d70950cbe443d46f3b9275&units=metric
+
+const APIKey = 'e19ae291c8d70950cbe443d46f3b9275';
+const APIURL = 'https://api.openweathermap.org/data/2.5/weather?id=london&appid=' + APIKey + '&units=metric';
+
+console.log(APIURL);
