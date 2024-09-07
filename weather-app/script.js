76c403ee984cc30ad6cc88e88a425ad35711d34a
mvpcurtis <curtis.bowen@gmx.com>
2024-09-07 18:31:31 +0100

    implemented some more function to the app

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,6 +1,22 @@
 // https://api.openweathermap.org/data/2.5/weather?id=london&appid=e19ae291c8d70950cbe443d46f3b9275&units=metric
 
-const APIKey = 'e19ae291c8d70950cbe443d46f3b9275';
-const APIURL = 'https://api.openweathermap.org/data/2.5/weather?id=london&appid=' + APIKey + '&units=metric';
+const APIKey = config.APIKey;
+const APIURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=London';
 
-console.log(APIURL);
+const city = document.querySelector('.city');
+const wind = document.querySelector('.wind');
+
+// console.log(APIURL + `&appid=${APIKey}`);
+
+async function checkWeather() {
+  const response = await fetch(APIURL + `&appid=${APIKey}`);
+  var data = await response.json();
+
+  console.log(data);
+  city.textContent = data.name;
+  wind.textContent = `${data.wind.speed} km/h`;
+}
+
+checkWeather();
+
+console.log(APIKey);
