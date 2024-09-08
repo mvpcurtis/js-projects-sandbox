662e0ffaeed660f59d9994201668d97d223674fd
mvpcurtis <curtis.bowen@gmx.com>
2024-09-08 19:37:03 +0100

    added if conditions for the images

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,5 +1,4 @@
 const APIKey = config.APIKey;
-let APICity = 'London';
 const APIURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
 
 let city = document.querySelector('.city');
@@ -10,26 +9,44 @@ const weatherIcon = document.querySelector('.weather-icon');
 const input = document.getElementById('input');
 const button = document.querySelector('button');
 
-const capFirstLetter = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+button.addEventListener('click', () => {
+  checkWeather(input.value);
+});
 
-const searchCity = () => {
-  APICity = capFirstLetter(input.value);
-  console.log(APICity);
-  checkWeather();
-};
-
-button.addEventListener('click', searchCity);
-
-async function checkWeather() {
-  console.log(APIURL + `&q=${APICity}` + `&appid=${APIKey}`);
-  const response = await fetch(APIURL + `&q=${APICity}` + `&appid=${APIKey}`);
+async function checkWeather(searchCity) {
+  console.log(APIURL + `&q=${searchCity}` + `&appid=${APIKey}`);
+  const response = await fetch(APIURL + `&q=${searchCity}` + `&appid=${APIKey}`);
   let data = await response.json();
   console.log(data);
-  city.textContent = APICity;
+  city.textContent = data.name;
   temp.textContent = `${Math.round(data.main.temp)}°C`;
   wind.textContent = `${data.wind.speed} km/h`;
   humidity.textContent = `${data.main.humidity}%`;
+
+  const conditions = data.weather[0].main.toLowerCase();
+  if (conditions === 'clear') {
+    weatherIcon.src = './images/clear.png';
+  } else if (conditions === 'clouds') {
+    weatherIcon.src = './images/clouds.png';
+  } else if (conditions === 'drizzle') {
+    weatherIcon.src = './images/drizzle.png';
+  } else if (conditions === 'mist') {
+    weatherIcon.src = './images/mist.png';
+  } else if (conditions === 'rain') {
+    weatherIcon.src = './images/rain.png';
+  } else if (conditions === 'snow') {
+    weatherIcon.src = './images/snow.png';
+  }
 }
 // console.log(APIURL + `&appid=${APIKey}`);
+
+/*
+
+chicken chow mein
+mushroom curry
+satay sauce / swap
+egg fried rice
+chicken balls
+chips
+
+*/
