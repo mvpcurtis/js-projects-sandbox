190a3c214517bbe54e607e4b153dba9bd715f091
mvpcurtis <curtis.bowen@gmx.com>
2024-10-20 11:18:25 +0100

    changed some logic

--- blackjack-app/script.js
+++ blackjack-app/script.js
@@ -1,60 +1,29 @@
-const hitBtn = document.querySelector('#hit');
-const stayBtn = document.querySelector('#stay');
+const cardElement = document.querySelector('#cards');
+const sumElement = document.querySelector('#sum');
 
-let cardOne;
-let cardTwo;
-let sum;
+const randomNumber = function () {
+  return Math.floor(Math.random() * (11 - 2) + 2);
+};
 
-function randomNumer() {
-  cardOne = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-  console.log(cardOne);
-  cardTwo = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-  console.log(cardTwo);
-}
+const cards = [randomNumber(), randomNumber()];
+let cardSum = 0;
 
-function startGame() {
-  randomNumer();
-  sum = cardOne + cardTwo;
-  let cardThree;
-
-  if (sum < 21) {
-    console.log('What would you like to do?');
-    hitBtn.addEventListener('click', function () {
-      let cardFour;
-      cardThree = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-      sum += cardThree;
-      console.log(cardThree, sum);
-
-      if (sum < 21) {
-        console.log('What would you like to do now?');
-        hitBtn.addEventListener('click', function () {
-          cardFour = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-          sum += cardFour;
-          console.log(cardFour, sum);
-        });
-
-        stayBtn.addEventListener('click', function () {
-          console.log('You have stayed with' + ' ' + sum);
-        });
-      } else if (sum === 21) {
-        console.log('Black Jack!');
-      } else if (sum > 21) {
-        console.log('Bust!');
-      }
-    });
-    stayBtn.addEventListener('click', function () {
-      console.log('You have stayed with' + ' ' + sum);
-    });
-  } else if (sum === 21) {
-    console.log('Black Jack!');
-  } else if (sum > 21) {
-    console.log('Bust!');
+const renderGame = function () {
+  cardElement.textContent = 'Cards: ';
+  sumElement.textContent = 'Sum: ';
+  for (let i = 0; i < cards.length; i++) {
+    cardElement.textContent += ' ' + cards[i];
+    cardSum += cards[i];
   }
-}
 
-function clear() {
-  document.querySelector('.buttons').style.display = 'block';
-  document.querySelector('#start').style.display = 'none';
-}
+  sumElement.textContent += cardSum;
+};
 
-document.querySelector('#start').addEventListener('click', clear);
+const startGame = function () {
+  document.querySelector('.ingame-buttons').style.display = 'block';
+  document.querySelector('#ingame').style.display = 'block';
+  document.querySelector('#start-game').style.display = 'none';
+  renderGame();
+};
+
+document.querySelector('#start-game').addEventListener('click', startGame);
